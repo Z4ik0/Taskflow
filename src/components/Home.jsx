a8d7608e4fe630c470827baca28074543d1c92ee
Irving Cruz Chávez <ic3386941@gmail.com>
2025-03-28 19:48:20 -0600

    Primera integracion entre componentes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import appFirebase from "../credenciales.js";
-import {getAuth, signOut} from 'firebase/auth';
-const auth = getAuth(appFirebase)
+import { getAuth, signOut } from "firebase/auth";
+import { useState } from "react";
+import TaskCalendar from "../calendar";
+import Welcome from "./Welcome";
+const auth = getAuth(appFirebase);
 
-const Home = ({correoUsusario}) => {
-    return (
+const Home = ({ correoUsusario }) => {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  const handleStart = () => {
+    setShowWelcome(false);
+  };
+
+  return (
     <div>
-        <h1 className="center">Bienbenido usuario{correoUsusario} <button className="btn-primary" onClick={()=>signOut(auth)}>Logout</button></h1>
+      <h1 className="center">
+        Bienbenido usuario{correoUsusario}{" "}
+        <button className="btn-primary" onClick={() => signOut(auth)}>
+          Logout
+        </button>
+      </h1>
+      {showWelcome ? (
+        <Welcome onStart={handleStart} />
+      ) : (
+        <>
+          <h1 className="text-center my-4">Taskflow</h1>
+          <TaskCalendar />
+        </>
+      )}
     </div>
-    );
-}
+  );
+};
 
 export default Home;
